1f12c2de6b752aa61eec0fc0ff413f10bd69de09
virat-18-git <ksprddy6@gmail.com>
2024-12-24 00:40:16 +0530

    conversions

---
+++ day2.py
@@ -0,0 +1,82 @@
+# list
+# list=[1,8,55,213,"virat",88,[8,5,2]]
+# print(list[5])
+# list[4]="raina"
+# print(list)
+# print(list[5][2])
+
+# tuple
+# tuple=(9,4,3,1,6,7)
+
+# range
+rng=range(0,25)
+# rng=range(0,25,2)
+# rng=list(rng)
+# print(rng)
+
+# rng=range(25,0,-1)
+# rng=list(rng)
+# print(rng)
+
+# for i in range(0,25):
+#  print("hello")
+#  print(i)
+
+#  dictionaries
+dict={'1':'shiva','2':'raamu','3':'krishna'}
+dict['2']= 'vamshi'
+# print(dict['3'])
+# print(dict)
+
+
+# sett
+set={1,3,5,7,9,153,186,197,536,5,8,7,"harii","raam"}
+print(set)
+print(set)
+print(set)
+print(set)
+print(set)
+
+# None
+nn=None
+print(nn)
+print(type(nn))
+
+# conversions
+int1=10.8
+int1=float(int1)
+print(int1)
+int1=int(int1)
+print(int1)
+
+int2=6
+cmplx=complex(int2,8)
+print(cmplx)
+# print(int(cmplx))
+
+# boolean
+print(int(True))
+print(int(False))
+print(bool(5))
+print(bool(-5))
+print(bool(0))
+
+str1="shiva prasad"
+print(list(str1))
+print(tuple(str1))
+
+
+list=[1,5,9,7,5,3,4,6]
+print(str(list))
+
+
+# dict={'1':'hhh','2':'kkk','3':'lll'}
+# print(set(dict))
+
+# set={1,5,7,9,55,88}
+# print(dict(set))
+
+
+
+
+
